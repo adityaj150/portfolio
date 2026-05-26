3d41d7f45778bc1f68301df2888efaf1dd341905
Aditya Jadhav <jadhav2005.adi@gmail.com>
2026-05-26 15:27:42 +0530

    added bird and plane model

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,8 @@ import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
 import Sky from '../models/Sky'
+import Bird from '../models/Bird'
+import Plane from '../models/Plane'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
@@ -47,6 +49,8 @@ const Home = () => {
             position={islandPosition}
             rotation={islandRotation}
           />
+          <Bird />
+          <Plane />
         </Suspense>
       </Canvas>
     </section>
