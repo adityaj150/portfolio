6ff77a8bc7980ccacc198bdbe67109aced8fe9be
Aditya Jadhav <jadhav2005.adi@gmail.com>
2026-05-29 01:11:21 +0530

    Added mouse and touch functionality in island.jsx and Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
@@ -11,6 +11,8 @@ import Plane from '../models/Plane'
 </div> */}
 
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+
   const adjustIslandForScreenSize = () => {
     let screenScale;
     let screenPosition;
@@ -31,12 +33,30 @@ const Home = () => {
     };
   };
 
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition;
+  
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screenScale = [3,3,3];
+      screenPosition = [0, -4, -4];
+    }
+  
+    return {
+      planeScale: screenScale,
+      planePosition: screenPosition
+    };
+  };
+
   const { islandScale, islandPosition, islandRotation } = adjustIslandForScreenSize();
+  const { planeScale, planePosition} = adjustPlaneForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
       <Canvas 
-        className="w-full h-full" 
+        className={`w-full h-full bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}  
         camera={{ near: 0.1, far: 1000 }}>
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1,1,1]} intensity={2} />
@@ -48,9 +68,16 @@ const Home = () => {
             scale={islandScale}
             position={islandPosition}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating} 
           />
           <Bird />
-          <Plane />
+          <Plane 
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0,20,0]}
+          />
         </Suspense>
       </Canvas>
     </section>
